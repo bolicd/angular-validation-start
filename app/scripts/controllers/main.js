fb787fc4c660d6fe3b32f7704a1f54adff466643
Damir Bolic <bolicd@gmail.com>
2016-03-29 14:38:36 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularValidationTutorialStartApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularValidationTutorialStartApp
+ */
+angular.module('angularValidationTutorialStartApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
